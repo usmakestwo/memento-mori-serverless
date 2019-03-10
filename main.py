d6cb9a9ba80dc8903e2ad916d30e1c61c9a39359
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-03-09 19:31:07 -0500

    feat: 🎸 add text file to modularize template

--- main.py
+++ main.py
@@ -2,31 +2,34 @@ import os
 from github import Github
 from template.template_builder import TemplateBuilder
 
-# First create a Github instance:
+# First create a Github instance
 g = Github(os.environ["ACCESS_TOKEN"])
 org = os.environ["ORG"]
 
 # Get Org
 organization = g.get_organization(org)
 
-def createRepo(name="Sample", description=":school_satchel: Learning Something New"):
+def createRepo(name="Sample", description="Learning A new skill"):
+  github_name = name.replace(" ", "-").lower()
+  print('Creating project ' + github_name)
   repo = organization.create_repo(
-    name,
+    name=github_name,
     allow_rebase_merge=True,
     auto_init=False,
-    description=description,
+    description=':school_satchel: ' + description,
     has_issues=True,
     has_projects=True,
     has_wiki=True,
     private=False,
   )
-  repo = g.get_repo(org + "/" + name)
+  
+  repo = g.get_repo(org + "/" + name.replace(" ", "-"))
   commit_message = "chore :robot: automated init"
   
   repo.create_file("README.md", commit_message, TemplateBuilder().init_readme(name), branch="master")
   repo.create_file("resources/README.md", commit_message, TemplateBuilder().init_resource_readme(name), branch="master")
   repo.create_file("courses/README.md", commit_message, TemplateBuilder().init_courses_readme(name), branch="master")
-  repo.create_file(".school.yml", commit_message, "test", branch="master")
+  repo.create_file("praxi.yaml", commit_message, TemplateBuilder().init_praxi(github_name, name, description), branch="master")
 
 # Create Repo
-createRepo()
+createRepo("Learning French", "Learning basic conversational french")
