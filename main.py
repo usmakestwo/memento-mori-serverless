9e3f0377311ba95fc65dd99f70502c40a12b60b9
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-03-17 22:45:23 -0400

    feat: 🎸 add github and db client to api endpoint

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-import os
-from github import Github
-from flask import Flask, Response
-from utils.template_builder import TemplateBuilder
+from flask import Flask, Response, request, jsonify
 from utils.connect_db import Datastore
+from utils.connect_github import GithubClient
 
-# First create a Github instance
-g = Github(os.environ["ACCESS_TOKEN"])
-org = os.environ["ORG"]
-
-# Get Org
-organization = g.get_organization(org)
-
-# Database
+# Databases
 db = Datastore()
+git = GithubClient()
 
 # Initialize Flask
 app = Flask(__name__)
@@ -26,30 +18,19 @@ def fetchProjects():
   projects = db.return_all_records()
   return Response(projects, status=200, mimetype='application/json')
 
-def createRepo(name="Sample", description="Learning A new skill"):
-  github_name = name.replace(" ", "-").lower()
-  print('Creating project ' + github_name)
-  repo = organization.create_repo(
-    name=github_name,
-    allow_rebase_merge=True,
-    auto_init=False,
-    description=':school_satchel: ' + description,
-    has_issues=True,
-    has_projects=True,
-    has_wiki=True,
-    private=False,
-  )
-  
-  repo = g.get_repo(org + "/" + name.replace(" ", "-"))
-  commit_message = "chore :robot: automated init"
-  
-  repo.create_file("README.md", commit_message, TemplateBuilder().init_readme(name), branch="master")
-  repo.create_file("resources/README.md", commit_message, TemplateBuilder().init_resource_readme(name), branch="master")
-  repo.create_file("courses/README.md", commit_message, TemplateBuilder().init_courses_readme(name), branch="master")
-  repo.create_file("praxi.yaml", commit_message, TemplateBuilder().init_praxi(github_name, name, description), branch="master")
+@app.route("/api/project", methods = ['POST'])
+def createProject():
+  if request.is_json:
+    content = request.get_json()
+    response = git.createRepo(content['name'], content['description'])
+    if response:
+      db.insert_record(content['name'], content['description'])
+      return 'Project successfully created'
+    else:
+      return jsonify(text='Failed creating project'), 400
+  else:
+    return 'Not Valid JSON'
+  content = request.get_json()
+  print (content)
+  return 'JSON posted'
 
-  db.insert_record(name, description)
-
-# Create all projects
-
-#createRepo('test', 'test description')
---
+++ utils/connect_github.py
@@ -0,0 +1,36 @@
+import os
+from github import Github
+from utils.template_builder import TemplateBuilder
+
+class GithubClient():
+  def __init__(self):
+    # First create a Github instance
+    self.git = Github('cbfeea192340ca5e4087a6623343a1712e8421bd')
+    self.org = 'memento-mori-universitas'
+    # Get Org
+    self.organization = self.git.get_organization(self.org)
+
+  def createRepo(self, name="Sample", description="Learning A new skill"):
+    github_name = name.replace(" ", "-").lower()
+    print('Creating project ' + github_name)
+    try:
+      repo = self.organization.create_repo(
+        name=github_name,
+        allow_rebase_merge=True,
+        auto_init=False,
+        description=':school_satchel: ' + description,
+        has_issues=True,
+        has_projects=True,
+        has_wiki=True,
+        private=False,
+      )
+      repo = self.git.get_repo(self.org + "/" + name.replace(" ", "-"))
+      commit_message = "chore :robot: automated init"
+      print(repo)
+      repo.create_file("README.md", commit_message, TemplateBuilder().init_readme(name), branch="master")
+      repo.create_file("resources/README.md", commit_message, TemplateBuilder().init_resource_readme(name), branch="master")
+      repo.create_file("courses/README.md", commit_message, TemplateBuilder().init_courses_readme(name), branch="master")
+      repo.create_file("praxi.yaml", commit_message, TemplateBuilder().init_praxi(github_name, name, description), branch="master")
+      return True
+    except Exception as e:
+      print(e)
