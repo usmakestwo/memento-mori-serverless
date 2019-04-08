30be596ffadac8e853cc762ea789650ad64723ed
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-04-07 22:26:01 -0400

    feat: 🎸 add path to repo

--- gcp-functions/gcp-write-courses/main.py
+++ gcp-functions/gcp-write-courses/main.py
@@ -7,24 +7,16 @@ db = Datastore()
 git = GithubClient()
 
 def create_project(request):
-  response.headers.set('Access-Control-Allow-Origin', '*')
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
   if request.is_json:
     content = request.get_json()
-    response = git.createRepo(content['name'], content['description'])
-    if response:
-      db.insert_record(content['name'], content['description'])
+    git_response = git.createRepo(content['name'], content['description'])
+    if git_response:
+      db.insert_record(content['name'], content['description'], git_response)
       response = jsonify(text='Project successfully created'), 200
-      response.headers.set('Access-Control-Allow-Origin', '*')
-      response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
       return response
     else:
       response = jsonify(text='Failed creating project'), 400
-      response.headers.set('Access-Control-Allow-Origin', '*')
-      response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
       return response
   else:
     response = jsonify(text='Not valid JSON'), 400
-    response.headers.set('Access-Control-Allow-Origin', '*')
-    response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
     return response
--- gcp-functions/gcp-write-courses/utils/connect_db.py
+++ gcp-functions/gcp-write-courses/utils/connect_db.py
@@ -10,12 +10,12 @@ class Datastore:
     password = os.environ["MONGO_PASS"]
     self.url = "mongodb://{}:{}@cluster0-shard-00-00-rpoft.gcp.mongodb.net:27017,cluster0-shard-00-01-rpoft.gcp.mongodb.net:27017,cluster0-shard-00-02-rpoft.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true".format(username, password)
 
-  def insert_record(self, name, description, uploaded=True):
+  def insert_record(self, name, description, path, uploaded=True):
     client = pymongo.MongoClient(self.url)
     mydb = client["universitas_library"]
     mycol = mydb["projects"]
 
-    single_project = {"createdAt": datetime.datetime.now(), "name": name, "description": description, "uploaded": True}
+    single_project = {"createdAt": datetime.datetime.now(), "name": name, "description": description, "path": "https://github.com/" + path.lower(), "uploaded": True}
 
     x = mycol.insert_one(single_project)
     return x.inserted_id
