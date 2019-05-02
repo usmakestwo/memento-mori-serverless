34d03a7e915dffe1aff21e66769200ee2a7a46a2
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-05-02 18:32:29 -0400

    feat: 🎸 return kanban with project

--- gcp-functions/gcp-read-courses/main.py
+++ gcp-functions/gcp-read-courses/main.py
@@ -11,10 +11,41 @@ def return_all_records(request):
   url = "mongodb://{}:{}@cluster0-shard-00-00-rpoft.gcp.mongodb.net:27017,cluster0-shard-00-01-rpoft.gcp.mongodb.net:27017,cluster0-shard-00-02-rpoft.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true".format(username, password)
   client = pymongo.MongoClient(url)
   mydb = client["universitas_library"]
-  mycol = mydb["projects"]
+  kanban = mydb["kanban"]
+  projects = mydb["projects"]
   collection = []
-  for record in mycol.find():
-    collection.append(record)
+
+  for x in kanban.find():
+    collection.append(x)
+
+  # Store each card reference from kanban project
+  for x in kanban.find({ "id": "wip" }):
+    wip_projects = x['cards']
+
+  for x in kanban.find({ "id": "learned" }):
+    learned_projects = x['cards']
+
+  for x in kanban.find({ "id": "backlog" }):
+    backlog_projects = x['cards']
+
+  for x in kanban.find({ "id": "practice" }):
+    practice_projects = x['cards']
+
+  wip_projects = list(projects.find({"_id": {"$in": wip_projects }}))
+  learned_projects = list(projects.find({"_id": {"$in": learned_projects }}))
+  backlog_projects = list(projects.find({"_id": {"$in": backlog_projects }}))
+  practice_projects = list(projects.find({"_id": {"$in": practice_projects }}))
+
+  for item in range(len(collection)):
+    if collection[item]["id"] == "backlog":
+      collection[item]["cards"] = backlog_projects
+    elif collection[item]["id"] == "wip":
+      collection[item]["cards"] = wip_projects
+    elif collection[item]["id"] == "learned":
+      collection[item]["cards"] = learned_projects
+    elif collection[item]["id"] == "practice":
+      collection[item]["cards"] = practice_projects
+
   response = jsonify(json.dumps(collection, default=json_util.default))
   response.headers.set('Access-Control-Allow-Origin', '*')
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST')
