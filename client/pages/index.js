c4b44308404532d65a05c0b15338eaee21dba704
gonzalovazquez <gonzalovazquez010@gmail.com>
2019-05-15 19:45:56 -0400

    ci: 🎡 added support to deploy via Zeit

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
